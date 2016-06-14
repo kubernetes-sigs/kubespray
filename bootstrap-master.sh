b009ca3ff8ef375d48709aafc047ea3a00ac2672
Aleksandr Didenko <adidenko@mirantis.com>
2016-06-14 19:10:20 +0200

    Fix inv dir copy command

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -6,7 +6,7 @@ sudo apt-get --yes upgrade
 sudo apt-get --yes install ansible git screen vim
 sudo git clone https://github.com/kubespray/kargo /root/kargo
 sudo git clone https://github.com/adidenko/vagrant-k8s /root/vagrant-k8s
-sudo cp -a /root/vagrant-k8s/inv /root/kargo/inv
+sudo cp -a /root/vagrant-k8s/kargo/inv /root/kargo/inv
 
 sudo rm -rf /root/.ssh
 sudo mv ~vagrant/ssh /root/.ssh

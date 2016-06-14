d365fab9ec17defbed5d127ee727332806f09200
Aleksandr Didenko <adidenko@mirantis.com>
2016-06-14 18:51:43 +0200

    Add inventory.cfg download to bootstrap script

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -5,6 +5,7 @@ sudo apt-get --yes update
 sudo apt-get --yes upgrade
 sudo apt-get --yes install ansible git screen vim
 sudo git clone https://github.com/kubespray/kargo /root/kargo
+sudo wget https://raw.githubusercontent.com/adidenko/vagrant-k8s/master/kargo/inventory.cfg -O /root/kargo/inventory/inventory.cfg
 
 sudo rm -rf /root/.ssh
 sudo mv ~vagrant/ssh /root/.ssh

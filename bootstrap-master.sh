d894529f07d27a1bd524af932f734626c61715e5
Aleksandr Didenko <adidenko@mirantis.com>
2016-06-14 18:30:30 +0200

    Update ssh/config on master node

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -8,5 +8,5 @@ sudo git clone https://github.com/kubespray/kargo /root/kargo
 
 sudo rm -rf /root/.ssh
 sudo mv ~vagrant/ssh /root/.ssh
+sudo echo -e 'Host 10.210.*\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null' >> /root/.ssh/config
 sudo chown -R root: /root/.ssh
-

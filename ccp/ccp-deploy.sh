5cc37db4bfd0974ab7ff6ec42f6a9e0acba7a66a
Aleksandr Didenko <adidenko@mirantis.com>
2016-06-28 16:54:34 +0200

    Minor improvements in nodes list generation

--- ccp/ccp-deploy.sh
+++ ccp/ccp-deploy.sh
@@ -3,10 +3,15 @@
 set -e
 
 create_network_conf() {
-  ( echo "network:"; i=2; for ip in `cat /root/nodes `; do echo -e "  node$i:\n    private:\n      iface: eth2\n      address: $ip"; pip=`echo $ip | perl -pe 's/(\d+).(\d+).1/\${1}.\${2}.0/g'`; echo -e "    public:\n      iface: eth1\n      address: $pip" ; i=$(( i+=1 )) ;done ) > /root/cluster-topology.yaml
+  # FIXME: hardcoded network topology:
+  #   public_ip:  \d+.\d+.0.\d+  pubcic_iface:  eth1
+  #   private_ip: \d+.\d+.1.\d+  private_iface: eth2
+  kubectl get nodes -o go-template='{{range .items}}{{range .status.addresses}}{{if or (eq .type "ExternalIP") (eq .type "LegacyHostIP")}}{{.address}}{{print "\n"}}{{end}}{{end}}{{end}}'> /tmp/nodes
+  ( echo "network:"; i=2; for ip in `cat /tmp/nodes `; do echo -e "  node$i:\n    private:\n      iface: eth2\n      address: $ip"; pip=`echo $ip | perl -pe 's/(\d+).(\d+).1/\${1}.\${2}.0/g'`; echo -e "    public:\n      iface: eth1\n      address: $pip" ; i=$(( i+=1 )) ;done ) > /root/cluster-topology.yaml
 }
 
 assign_node_roles() {
+  # FIXME: hardcoded roles
   kubectl label nodes node2 openstack-controller=true
   kubectl label nodes node3 openstack-controller=true
   kubectl label nodes node4 openstack-controller=true

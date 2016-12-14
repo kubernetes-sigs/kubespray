ad68b23d8a6bbb39892570855768a9c6a63dca93
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-12-14 17:00:38 +0100

    Manual steps for Gitlab CI pipeline
    * Reduce default testcase to 2 nodes, add HA case.
    * Adjust gen_matrix script for Travis/Gitlab CIs.
    * Enable netchecker deploy foro gitlab CI.
    * Sync other things from travis matrix and reorder them as build steps
      for pull requests, master branch, auto/manual.
    * Do auto-step1 from part1 and manual step2,3 for branches/PRs.
    * Do manual steps from part2, special for master merges.
    
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- scripts/gen_matrix.sh
+++ scripts/gen_matrix.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 # Generates MD formatted CI matrix from the .travis.yml
-a=$(perl -ne '/^\s{6}(CLOUD_IMAGE|KUBE_NETWORK_PLUGIN|CLOUD_REGION|CLUSTER_MODE)=(\S+)$/ && print "$2\n"' .travis.yml)
+a=$(perl -ne '/^\s{6}(CLOUD_IMAGE|KUBE_NETWORK_PLUGIN|CLOUD_REGION|CLUSTER_MODE)=(\S+)$/ && print "$2\n"' .travis.yml.bak)
+echo Travis-CI
 printf "|%25s|%25s|%25s|%25s|\n" "Network plugin" "OS type" "GCE region" "Nodes layout"
 echo "|-------------------------|-------------------------|-------------------------|-------------------------|"
 c=0
 for i in `echo $a`; do
   printf "|%25s" $i
   [ $(($c % 4)) -eq 3 ] && printf "|\n"
+  c=$(( c + 1))
+done
+
+echo
+a=$(perl -ne '/^\s{2}(stage:\sdeploy-gce-|when:\s)(\S+)$/ && print "$2\n";/^\s{4}(CLOUD_IMAGE|KUBE_NETWORK_PLUGIN|CLOUD_REGION|CLUSTER_MODE):\s(\S+)$/ && print "$2\n"' .gitlab-ci.yml)
+echo Gitlab-CI
+printf "|%18s|%18s|%18s|%18s|%18s|%18s|\n"  "Stage" "Network plugin" "OS type" "GCE region" "Nodes layout" "When"
+echo "|------------------|------------------|------------------|------------------|------------------|------------------|"
+c=0
+for i in `echo $a`; do
+  printf "|%18s" $i
+  [ $(($c % 6)) -eq 5 ] && printf "|\n"
   c=$(( c + 1))
 done

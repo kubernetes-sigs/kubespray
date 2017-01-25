2333ec4d1f013845994103ee9faf31df88404d8f
Brad Beam <brad.beam@b-rad.info>
2017-01-24 22:44:06 -0600

    Splitting out moderator check from syntax check
    - Attempt to clarify CI runs from contributors

--- scripts/premoderator.sh
+++ scripts/premoderator.sh
@@ -11,5 +11,8 @@ issue=$(echo ${CI_BUILD_REF_NAME} | perl -ne '/^pr-(\d+)-\S+$/ && print $1')
 user=$(curl ${CURL_ARGS} "https://api.github.com/repos/kubernetes-incubator/kargo/issues/${issue}/comments" \
   | jq -M "map(select(.body | contains (\"$MAGIC\"))) | .[0] .user.login" | tr -d '"')
 # Check for the required user group membership to allow (exit 0) or decline (exit >0) the pipeline
-[ "$user" != "null" ] || exit 1
+if [ "$user" = "null" ]; then
+	echo "User does not have permissions to start CI run"
+	exit 1
+fi
 curl ${CURL_ARGS} "https://api.github.com/orgs/kubernetes-incubator/members/${user}"

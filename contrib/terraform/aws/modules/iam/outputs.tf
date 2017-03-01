5b90fa0f668269f228600c17d6e15cc3e25820db
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2017-03-01 18:25:58 +0100

    Rewrote AWS Terraform for Kargo
    Rewrote AWS Terraform deployment for AWS Kargo. It supports now
    multiple Availability Zones, AWS Loadbalancer for Kubernetes API,
    Bastion Host, ...
    
    For more information see README

---
+++ contrib/terraform/aws/modules/iam/outputs.tf
@@ -0,0 +1,7 @@
+output "kube-master-profile" {
+    value = "${aws_iam_instance_profile.kube-master.name }"
+}
+
+output "kube-worker-profile" {
+    value = "${aws_iam_instance_profile.kube-worker.name }"
+}

e34ffb39f13435c203d243dab3a11ad302820c5c
Martin Uddén <martin.udden@gmail.com>
2017-08-04 15:08:20 +0200

    feature: install project atomic CSS on RedHat family

---
+++ roles/docker/files/install_container_storage_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+version=${1:-master}
+profile_name=${2:-kubespray}
+dir=`mktemp -d`
+export GIT_DIR=$dir/.git
+export GIT_WORK_TREE=$dir
+
+git init
+git fetch --depth 1 https://github.com/projectatomic/container-storage-setup.git $version
+git merge FETCH_HEAD
+make -C $dir install
+rm -rf /var/lib/container-storage-setup/$profile_name $dir
+
+set +e
+
+/usr/bin/container-storage-setup create $profile_name /etc/sysconfig/docker-storage-setup && /usr/bin/container-storage-setup activate $profile_name
+exit 0

3a9b4e4679ee9861f96dac748ca6e72cff618f1a
pasindu94 <33244732+pasindu94@users.noreply.github.com>
2018-10-09 08:53:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@
 #   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #   License for the specific language governing permissions and limitations
 #   under the License.
+#   python script for setup
 
 import setuptools
 

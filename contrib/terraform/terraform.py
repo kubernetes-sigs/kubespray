f02cb6e8c518bf21f5ea8c996cd036f739b57aeb
Ted Wexler <twexler@bloomberg.net>
2018-10-29 12:55:41 -0400

    If there's a floating IP assigned to an openstack instance, use that for access_ip

--- contrib/terraform/terraform.py
+++ contrib/terraform/terraform.py
@@ -686,6 +686,7 @@ def iter_host_ips(hosts, ips):
             ip = ips[host_id]
             host[1].update({
                 'access_ip_v4': ip,
+                'access_ip': ip,
                 'public_ipv4': ip,
                 'ansible_ssh_host': ip,
             })

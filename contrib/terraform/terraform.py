58b4fea2b19697b1671912932a095b7bb9a969e0
Ted Wexler <ted@stuckinacan.com>
2018-10-29 14:28:23 -0400

    Add an 'access_ip' for openstack resources to the terraform inventory builder script (#3592)
    * Add an 'access_ip' for openstack resources to the terraform inventory builder script
    
    * Update Openstack README
    
    * Only use ipv4
    
    * If there's a floating IP assigned to an openstack instance, use that for access_ip

--- contrib/terraform/terraform.py
+++ contrib/terraform/terraform.py
@@ -328,6 +328,7 @@ def openstack_host(resource, module_name):
     attrs = {
         'access_ip_v4': raw_attrs['access_ip_v4'],
         'access_ip_v6': raw_attrs['access_ip_v6'],
+        'access_ip': raw_attrs['access_ip_v4'],
         'ip': raw_attrs['network.0.fixed_ip_v4'],
         'flavor': parse_dict(raw_attrs, 'flavor',
                              sep='_'),
@@ -685,6 +686,7 @@ def iter_host_ips(hosts, ips):
             ip = ips[host_id]
             host[1].update({
                 'access_ip_v4': ip,
+                'access_ip': ip,
                 'public_ipv4': ip,
                 'ansible_ssh_host': ip,
             })

0800beb3127c1ab00c5dd2c4869395409ad77ac4
Ted Wexler <twexler@bloomberg.net>
2019-02-14 09:56:39 -0500

    Run 'terraform fmt' in contrib/terraform/openstack

--- contrib/terraform/openstack/sample-inventory/cluster.tf
+++ contrib/terraform/openstack/sample-inventory/cluster.tf
@@ -6,11 +6,13 @@ public_key_path = "~/.ssh/id_rsa.pub"
 
 # image to use for bastion, masters, standalone etcd instances, and nodes
 image = "<image name>"
+
 # user on the node (ex. core on Container Linux, ubuntu on Ubuntu, etc.)
 ssh_user = "<cloud-provisioned user>"
 
 # 0|1 bastion nodes
 number_of_bastions = 0
+
 #flavor_bastion = "<UUID>"
 
 # standalone etcds
@@ -18,14 +20,20 @@ number_of_etcd = 0
 
 # masters
 number_of_k8s_masters = 1
+
 number_of_k8s_masters_no_etcd = 0
+
 number_of_k8s_masters_no_floating_ip = 0
+
 number_of_k8s_masters_no_floating_ip_no_etcd = 0
+
 flavor_k8s_master = "<UUID>"
 
 # nodes
 number_of_k8s_nodes = 2
+
 number_of_k8s_nodes_no_floating_ip = 4
+
 #flavor_k8s_node = "<UUID>"
 
 # GlusterFS
@@ -40,7 +48,11 @@ number_of_k8s_nodes_no_floating_ip = 4
 
 # networking
 network_name = "<network>"
+
 external_net = "<UUID>"
+
 subnet_cidr = "<cidr>"
+
 floatingip_pool = "<pool>"
+
 bastion_allowed_remote_ips = ["0.0.0.0/0"]

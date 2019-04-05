2a54be53625798cb567eea11150987ad8b35c388
Andreas Krüger <ak@patientsky.com>
2019-04-05 10:06:32 +0200

    Fix indentation

--- scripts/premoderator.sh
+++ scripts/premoderator.sh
@@ -2,7 +2,7 @@
 # A naive premoderation script to allow Gitlab CI pipeline on a specific PRs' comment
 # Exits with 0, if the pipeline is good to go
 # Exits with 1, if the user is not allowed to start pipeline
-# Exits with 2, if script is unable to get issue id from $CI_BUILD_REF_NAME variable
+# Exits with 2, if script is unable to get issue id from CI_BUILD_REF_NAME variable
 # Exits with 3, if missing the magic comment in the pipeline to start the pipeline
 
 CURL_ARGS="-fs --retry 4 --retry-delay 5"
@@ -24,8 +24,8 @@ user=$(curl ${CURL_ARGS} "https://api.github.com/repos/kubernetes-sigs/kubespray
 
 # Check for the required user group membership to allow (exit 0) or decline (exit >0) the pipeline
 if [ "$user" = "" ] || [ "$user" = "null" ]; then
-	echo "Missing '$MAGIC' comment from one of the OWNERS"
-	exit 3
+  echo "Missing '$MAGIC' comment from one of the OWNERS"
+  exit 3
 else
   echo "Found comment from user: $user"
 fi
@@ -34,7 +34,7 @@ curl ${CURL_ARGS} "https://api.github.com/orgs/kubernetes-sigs/members/${user}"
 
 if [ $? -ne 0 ]; then
   echo "User does not have permissions to start CI run"
-	exit 1
+  exit 1
 else
   echo "$user has allowed CI to start"
 fi

cd2dce2629dfd64c38ac55e4a0a6968cc8702c7c
MarkusTeufelberger <mteufelberger@mgit.at>
2019-04-08 09:28:24 +0200

    Update pypy3 on CoreOS to 7.0.0 (#4456)

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -11,7 +11,7 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=6.0.0
+PYPY_VERSION=7.0.0
 
 wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
 mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3

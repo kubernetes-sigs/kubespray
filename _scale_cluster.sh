9cf722d479de3dff4d72fc878de07c34d726471a
Sergey Bondarev <s.bondarev@southbridge.ru>
2019-04-10 18:54:21 +0300

    slurm sample inventory and helper deploy scripts

---
+++ _scale_cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+###ssh-agent bash
+#ssh-add ~/.ssh/id_rsa
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 adminname"
+  exit 1
+fi
+
+d=$(date '+%Y.%m.%d_%H:%M')
+
+export ANSIBLE_LOG_PATH=./deploy-$d.log
+ansible-playbook -u $1 -k -i inventory/s000/inventory.ini scale.yml -b --diff

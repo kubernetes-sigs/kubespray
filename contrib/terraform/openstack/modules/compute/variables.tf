b7da21fdc82aac7359666840c343d0e2e7f05710
Andreas Holmsten <andreas.holmsten@gmail.com>
2019-04-15 16:22:08 +0200

    [contrib/terraform/openstack] Add k8s_allowed_remote_ips variable (#4506)
    * Add k8s_allowed_remote_ips variable
    
    Useful for defining CIDRs allowed to initiate a SSH connection when
    you don't want to use a bastion.
    
    * Add TF_VAR_k8s_allowed_remote_ips variable to tf-apply-ovh

--- contrib/terraform/openstack/modules/compute/variables.tf
+++ contrib/terraform/openstack/modules/compute/variables.tf
@@ -66,6 +66,10 @@ variable "bastion_allowed_remote_ips" {
   type = "list"
 }
 
+variable "k8s_allowed_remote_ips" {
+  type = "list"
+}
+
 variable "supplementary_master_groups" {
   default = ""
 }

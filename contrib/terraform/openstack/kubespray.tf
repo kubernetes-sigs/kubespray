241ff3911f2603273ffbdf38857f1963b08137b5
rptaylor <rptaylor@uvic.ca>
2019-04-21 01:57:44 -0700

    add master_allowed_remote_ips (with terraform fmt) (#4022)

--- contrib/terraform/openstack/kubespray.tf
+++ contrib/terraform/openstack/kubespray.tf
@@ -52,6 +52,7 @@ module "compute" {
   k8s_node_fips                                = "${module.ips.k8s_node_fips}"
   bastion_fips                                 = "${module.ips.bastion_fips}"
   bastion_allowed_remote_ips                   = "${var.bastion_allowed_remote_ips}"
+  master_allowed_remote_ips                    = "${var.master_allowed_remote_ips}"
   k8s_allowed_remote_ips                       = "${var.k8s_allowed_remote_ips}"
   k8s_allowed_egress_ips                       = "${var.k8s_allowed_egress_ips}"
   supplementary_master_groups                  = "${var.supplementary_master_groups}"

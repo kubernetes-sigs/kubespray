f1d851197aeb9ab19a5d2cb6b94e1191cc06161e
Pablo Martinez Schroder <pmartinez@theworkshop.com>
2019-05-13 23:40:54 +0200

    Removing hardcoded pypy reference so it can be treated as a _download_url setting

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -11,10 +11,8 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=7.0.0
-
-wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
-mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3
+wget -O - $PYPY_URL | tar -xjf -
+mv -n $PYPY_FILE pypy3
 
 ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version

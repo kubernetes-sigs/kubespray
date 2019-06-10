81d1c569a97816a44c1b86916d54c7b2104416c8
Maxime Guyot <Miouge1@users.noreply.github.com>
2019-06-10 21:17:05 +0300

    Rebase only on PRs (#4861)

--- tests/scripts/rebase.sh
+++ tests/scripts/rebase.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Rebase on master to get latest changes
-git config user.email "ci@kubespray.io"
-git config user.name "CI"
-git pull --rebase origin master
+# Rebase PRs on master to get latest changes
+if [[ $CI_COMMIT_REF_NAME == pr-* ]]; then
+  git config user.email "ci@kubespray.io"
+  git config user.name "CI"
+  git pull --rebase origin master
+fi

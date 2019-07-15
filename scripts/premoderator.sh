37db872c202892ed5aa2226c597b5a207cfd9793
Andreas Kruger <ak@patientsky.com>
2019-07-15 16:36:49 +0200

    Minor fixes

--- scripts/premoderator.sh
+++ scripts/premoderator.sh
@@ -7,6 +7,7 @@
 
 CURL_ARGS="-fs --retry 4 --retry-delay 5"
 MAGIC="${MAGIC:-ci check this}"
+exit_code=0
 
 # Get PR number from CI_BUILD_REF_NAME
 issue=$(echo ${CI_BUILD_REF_NAME} | perl -ne '/^pr-(\d+)-\S+$/ && print $1')
@@ -17,31 +18,31 @@ if [ "$issue" = "" ]; then
 fi
 
 echo "Fetching labels from PR $issue"
-labels=$(curl -skL "https://api.github.com/repos/kubernetes-sigs/kubespray/issues/${issue}?access_token=${GITHUB_TOKEN}" | jq '{labels: .labels}' | jq '.labels[].name' | jq -s '')
+labels=$(curl ${CURL_ARGS} "https://api.github.com/repos/kubernetes-sigs/kubespray/issues/${issue}?access_token=${GITHUB_TOKEN}" | jq '{labels: .labels}' | jq '.labels[].name' | jq -s '')
+labels_to_patch=$(echo $labels | jq '. + ["needs-ci-auth"]' | tr -d "\n" | sed 's/"/\\"/g')
 
 echo "Checking for '$MAGIC' comment in PR $issue"
 
 # Get the user name from the PR comments with the wanted magic incantation casted
-user=$(curl ${CURL_ARGS} "https://api.github.com/repos/kubernetes-sigs/kubespray/issues/${issue}/comments" \
-  | jq -M "map(select(.body | contains (\"$MAGIC\"))) | .[0] .user.login" | tr -d '"')
+user=$(curl ${CURL_ARGS} "https://api.github.com/repos/kubernetes-sigs/kubespray/issues/${issue}/comments" | jq -M "map(select(.body | contains (\"$MAGIC\"))) | .[0] .user.login" | tr -d '"')
 
 # Check for the required user group membership to allow (exit 0) or decline (exit >0) the pipeline
 if [ "$user" = "" ] || [ "$user" = "null" ]; then
   echo "Missing '$MAGIC' comment from one of the OWNERS"
-  exit 3
+  exit_code=3
 else
   echo "Found comment from user: $user"
-fi
 
-curl ${CURL_ARGS} "https://api.github.com/orgs/kubernetes-sigs/members/${user}"
+  curl ${CURL_ARGS} "https://api.github.com/orgs/kubernetes-sigs/members/${user}"
 
-if [ $? -ne 0 ]; then
-  echo "User does not have permissions to start CI run"
-  labels_to_patch=$(echo $labels | jq '. + ["needs-ci-auth"]' | tr -d "\n" | sed 's/"/\\"/g')
-else
-  labels_to_patch=$(echo $labels | jq '. - ["needs-ci-auth"]' | tr -d "\n" | sed 's/"/\\"/g')
-  exit_code=0
-  echo "$user has allowed CI to start"
+  if [ $? -ne 0 ]; then
+    echo "User does not have permissions to start CI run"
+    exit_code=1
+  else
+    labels_to_patch=$(echo $labels | jq '. - ["needs-ci-auth"]' | tr -d "\n" | sed 's/"/\\"/g')
+    exit_code=0
+    echo "$user has allowed CI to start"
+  fi
 fi
 
 # Patch labels on PR

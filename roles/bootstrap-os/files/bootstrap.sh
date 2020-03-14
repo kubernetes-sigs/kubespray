d81dd60b18a53a515a22caffc24676e45dfacca1
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2020-03-14 14:32:35 +0100

    Python bootstrap: upgrade pypy to 3.6-7.2.0. (#5511)
    Solves problem with mitogen about 'Compress object has no attribute copy' in zlib module.

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -11,10 +11,10 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=7.0.0
+PYPY_VERSION=7.2.0
 
-wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
-mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3
+wget -O - https://github.com/squeaky-pl/portable-pypy/releases/download/pypy3.6-7.2.0/pypy3.6-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
+mv -n pypy3.6-$PYPY_VERSION-linux_x86_64-portable pypy3
 
 ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version

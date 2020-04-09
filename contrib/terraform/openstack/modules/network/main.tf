2e672894731fff1134b0a5990cf0f9de2136f62e
qvicksilver <jonathan@sofiero.net>
2020-04-09 11:27:44 +0200

    Terraform/OpenStack: Fix idempotency bug in module.network.openstack_networking_router_interface_v2.k8s[0] (#5914)

--- contrib/terraform/openstack/modules/network/main.tf
+++ contrib/terraform/openstack/modules/network/main.tf
@@ -28,6 +28,6 @@ resource "openstack_networking_subnet_v2" "k8s" {
 
 resource "openstack_networking_router_interface_v2" "k8s" {
   count     = "${var.use_neutron}"
-  router_id = "%{if openstack_networking_router_v2.k8s != []}${openstack_networking_router_v2.k8s[count.index].id} %{else}${var.router_id} %{endif}"
+  router_id = "%{if openstack_networking_router_v2.k8s != []}${openstack_networking_router_v2.k8s[count.index].id}%{else}${var.router_id}%{endif}"
   subnet_id = "${openstack_networking_subnet_v2.k8s[count.index].id}"
 }

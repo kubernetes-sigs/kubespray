c600b79311202a3a22fc050c3d1622091c77edb6
Uday <uday@reynencourt.com>
2020-04-17 17:17:15 +0200

    added preflight checks

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t  kubespray:${1} .
-docker tag kubespray:${1} staging.repo.rcplatform.io/kubespray/kubespray:{1}
-docker push staging.repo.rcplatform.io/kubespray/kubespray:{1}
+docker tag kubespray:${1} staging.repo.rcplatform.io/kubespray/kubespray:${1}
+docker push staging.repo.rcplatform.io/kubespray/kubespray:${1}

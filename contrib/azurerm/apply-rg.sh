a9beb5af246d6ebe9ac9883a7e12b619be01da2d
Kenichi Omichi <ken1ohmichi@gmail.com>
2020-05-03 12:42:26 -0700

    Azure: Rename apply-rg_2.sh to apply-rg.sh (#6049)
    apply-rg.sh was for Azure command version 1("azure" command) and the
    command is old and version 2("az" command) is officially used today.
    apply-rg_2.sh was for the version 2. In addition, the README[1] says
    we need to run apply-rg.sh for applying templates.
    
    This renames apply-rg_2.sh to apply-rg.sh for common usages of the
    version 2.
    
    [1]: https://github.com/kubernetes-sigs/kubespray/tree/master/contrib/azurerm#generating-and-applying

--- contrib/azurerm/apply-rg.sh
+++ contrib/azurerm/apply-rg.sh
@@ -9,18 +9,11 @@ if [ "$AZURE_RESOURCE_GROUP" == "" ]; then
     exit 1
 fi
 
-if az &>/dev/null; then
-    echo "azure cli 2.0 found, using it instead of 1.0"
-    ./apply-rg_2.sh "$AZURE_RESOURCE_GROUP"
-elif azure &>/dev/null; then 
-    ansible-playbook generate-templates.yml
-    
-    azure group deployment create -f ./.generated/network.json -g $AZURE_RESOURCE_GROUP
-    azure group deployment create -f ./.generated/storage.json -g $AZURE_RESOURCE_GROUP
-    azure group deployment create -f ./.generated/availability-sets.json -g $AZURE_RESOURCE_GROUP
-    azure group deployment create -f ./.generated/bastion.json -g $AZURE_RESOURCE_GROUP
-    azure group deployment create -f ./.generated/masters.json -g $AZURE_RESOURCE_GROUP
-    azure group deployment create -f ./.generated/minions.json -g $AZURE_RESOURCE_GROUP
-else 
-    echo "Azure cli not found"
-fi
+ansible-playbook generate-templates.yml
+
+az deployment group create --template-file ./.generated/network.json -g $AZURE_RESOURCE_GROUP
+az deployment group create --template-file ./.generated/storage.json -g $AZURE_RESOURCE_GROUP
+az deployment group create --template-file ./.generated/availability-sets.json -g $AZURE_RESOURCE_GROUP
+az deployment group create --template-file ./.generated/bastion.json -g $AZURE_RESOURCE_GROUP
+az deployment group create --template-file ./.generated/masters.json -g $AZURE_RESOURCE_GROUP
+az deployment group create --template-file ./.generated/minions.json -g $AZURE_RESOURCE_GROUP

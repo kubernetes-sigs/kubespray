31094b1768f26dd3078f1aa01c24d29e959723d2
Florian Ruynat <florian234@hotmail.com>
2020-05-25 11:43:11 +0200

    Add new Kubernetes version hashes and set default to 1.17.6 (#6183)
    * Add new Kubernetes version hashes and set default to 1.17.6
    
    * Do not rebase against master when commiting a release branch

--- tests/scripts/rebase.sh
+++ tests/scripts/rebase.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Rebase PRs on master to get latest changes
+# Rebase PRs on release-2.13 to get latest changes
 if [[ $CI_COMMIT_REF_NAME == pr-* ]]; then
   git config user.email "ci@kubespray.io"
   git config user.name "CI"
-  git pull --rebase origin master
+  git pull --rebase origin release-2.13
 fi

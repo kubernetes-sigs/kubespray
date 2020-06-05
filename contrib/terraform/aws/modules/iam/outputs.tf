2f0da2db81cce909dea18de208908db765442327
Florian Ruynat <florian234@hotmail.com>
2020-06-05 09:05:43 +0200

    Terraform quoted references are now deprecated (#6203)

--- contrib/terraform/aws/modules/iam/outputs.tf
+++ contrib/terraform/aws/modules/iam/outputs.tf
@@ -1,7 +1,7 @@
 output "kube-master-profile" {
-  value = "${aws_iam_instance_profile.kube-master.name}"
+  value = aws_iam_instance_profile.kube-master.name
 }
 
 output "kube-worker-profile" {
-  value = "${aws_iam_instance_profile.kube-worker.name}"
+  value = aws_iam_instance_profile.kube-worker.name
 }

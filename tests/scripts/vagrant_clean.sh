7e292669007e06b5f3094b8d271b407578a4840c
Maxime Guyot <Miouge1@users.noreply.github.com>
2020-09-17 17:04:45 +0200

    Cleanup virsh volumes in Vagrant CI (#6688)

--- tests/scripts/vagrant_clean.sh
+++ tests/scripts/vagrant_clean.sh
@@ -10,3 +10,10 @@ do
     virsh destroy "$i"
     virsh undefine "$i"
 done
+
+
+# Cleanup domain volumes
+for i in $(virsh vol-list default|grep \.img |grep -v VAGRANTSLASH | cut -f 2 -d ' ')
+do
+    virsh vol-delete "$i" --pool default
+done

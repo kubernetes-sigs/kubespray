6a194f715a235a4838fadda07f28344c24f98c48
bogdan <bogdan@reynencourt.com>
2020-10-13 15:47:08 +0200

    modifies the source repos for docker images to lock down the platform depl.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t  kubespray:${1} . --no-cache
+docker build -t kubespray:${1} . --no-cache
 docker tag kubespray:${1} certified-registry.rcplatform.io/kubespray/kubespray:${1}
 docker push certified-registry.rcplatform.io/kubespray/kubespray:${1}

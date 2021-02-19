179aeea47e1f5e56694f95d48f20fe1ec2b9b10e
Hugo Blom <bl0m1@users.noreply.github.com>
2021-02-19 18:16:25 +0100

    use image id instad of name (#7293)

--- contrib/terraform/terraform.py
+++ contrib/terraform/terraform.py
@@ -323,7 +323,7 @@ def openstack_host(resource, module_name):
     })
 
     # add groups based on attrs
-    groups.append('os_image=' + attrs['image']['name'])
+    groups.append('os_image=' + attrs['image']['id'])
     groups.append('os_flavor=' + attrs['flavor']['name'])
     groups.extend('os_metadata_%s=%s' % item
                   for item in list(attrs['metadata'].items()))

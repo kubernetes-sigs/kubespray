a1a475d888684157e543d38c674946d5418e508f
Fredrik Liv <fredrik.liv@elastisys.com>
2021-10-07 19:11:46 +0200

    Added terraform script for Hetzner cloud (#8053)

---
+++ contrib/terraform/hetzner/modules/kubernetes-cluster/output.tf
@@ -0,0 +1,23 @@
+output "master_ip_addresses" {
+  value = {
+    for key, instance in hcloud_server.master :
+    instance.name => {
+      "private_ip" = hcloud_server_network.master[key].ip
+      "public_ip"  = hcloud_server.master[key].ipv4_address
+    }
+  }
+}
+
+output "worker_ip_addresses" {
+  value = {
+    for key, instance in hcloud_server.worker :
+    instance.name => {
+      "private_ip" = hcloud_server_network.worker[key].ip
+      "public_ip"  = hcloud_server.worker[key].ipv4_address
+    }
+  }
+}
+
+output "cluster_private_network_cidr" {
+  value = var.private_subnet_cidr
+}

31091c1f76ebc2ca2f99ec0c6c05cca2896873fe
Omar Aloraini <ooraini.dev@gmail.com>
2021-11-06 14:40:43 +0300

    Update testcases_prepare.sh for Ansible collection

--- tests/scripts/testcases_prepare.sh
+++ tests/scripts/testcases_prepare.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 /usr/bin/python -m pip uninstall -y ansible
 /usr/bin/python -m pip install -r tests/requirements.txt
+ansible-galaxy collection build --force .
+ansible-galaxy collection install --force kubernetes-kubespray-*.tar.gz
 mkdir -p /.ssh
 mkdir -p cluster-dump
 mkdir -p $HOME/.ssh

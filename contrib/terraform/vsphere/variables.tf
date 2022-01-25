d86a3b962cfc8ffde5bb511665baca55ff0f99ef
ceesios <cees@virtu-on.nl>
2022-01-25 14:24:30 +0100

    Proposing fixes for contrib/terraform/vsphere/ #8436 (#8441)
    * fixes issues in vSphere Terraform contrib. #8436
    
    * fix formatting
    
    * add variables to the main module and document changes
    
    * add missing newline

--- contrib/terraform/vsphere/variables.tf
+++ contrib/terraform/vsphere/variables.tf
@@ -27,8 +27,6 @@ variable "vsphere_password" {}
 
 variable "vsphere_server" {}
 
-variable "vsphere_hostname" {}
-
 variable "ssh_public_keys" {
   description = "List of public SSH keys which are injected into the VMs."
   type        = list(string)
@@ -37,6 +35,13 @@ variable "ssh_public_keys" {
 variable "template_name" {}
 
 # Optional variables (ones where reasonable defaults exist)
+variable "vapp" {
+  default = false
+}
+
+variable "interface_name" {
+  default = "ens192"
+}
 
 variable "folder" {
   default = ""

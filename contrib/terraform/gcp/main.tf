0b757f1d87696a7549e8d35f0c03131b616c89cf
Mathieu Parent <math.parent@gmail.com>
2022-02-28 10:56:37 +0100

    terraform/gcp: Add ingress_whitelist
    Also, do not create unneeded resources (target pools are charged and should
    only be created when needed).

--- contrib/terraform/gcp/main.tf
+++ contrib/terraform/gcp/main.tf
@@ -33,4 +33,5 @@ module "kubernetes" {
   ssh_whitelist        = var.ssh_whitelist
   api_server_whitelist = var.api_server_whitelist
   nodeport_whitelist   = var.nodeport_whitelist
+  ingress_whitelist    = var.ingress_whitelist
 }

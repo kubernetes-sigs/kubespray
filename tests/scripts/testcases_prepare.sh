ae916c70e5e6ef73d75f446a70813993e9cc9dd5
Kenichi Omichi <ken1ohmichi@gmail.com>
2022-05-02 11:34:31 -0700

    Use ansible 2.12 for testcases_prepare (#8763)
    tests/requirements.txt links to tests/requirements-2.12.txt, so
    Kubespray uses ansible 2.12 by default for testing. However we
    forgot to update testcases_prepare.sh to use ansible 2.12.
    This updates testcases_prepare to use ansible 2.12.

--- tests/scripts/testcases_prepare.sh
+++ tests/scripts/testcases_prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-: ${ANSIBLE_MAJOR_VERSION:=2.10}
+: ${ANSIBLE_MAJOR_VERSION:=2.12}
 
 /usr/bin/python -m pip uninstall -y ansible ansible-base ansible-core
 /usr/bin/python -m pip install -r tests/requirements-${ANSIBLE_MAJOR_VERSION}.txt

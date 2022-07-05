7414409aa0039dbc93aa913776a23ace5b82bab6
Kenichi Omichi <ken1ohmichi@gmail.com>
2022-07-05 08:02:47 -0700

    Add target components on check_readme_versions.sh (#9045)
    This adds target components on check_readme_versions.sh after
    merging https://github.com/kubernetes-sigs/kubespray/pull/9044
    In addition, this fixes typo on check_readme_versions.sh
    
    This adds `foo_version` variables for some components because
    check_readme_versions.sh verifies the corresponding version for
    `<component name>_version` from main.yml. This change also makes
    consistency in the main.yml. In long-term, we will be able to
    remove the existing `foo_image_tag` variables, but that is not now
    for backwards compatibility for users.

--- tests/scripts/check_readme_versions.sh
+++ tests/scripts/check_readme_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-TARGET_COMPONENTS="containerd calico cilium flannel kube-ovn kube-router weave cert-manager"
+TARGET_COMPONENTS="containerd calico cilium flannel kube-ovn kube-router weave cert-manager krew helm metallb registry cephfs-provisioner rbd-provisioner aws-ebs-csi-plugin azure-csi-plugin cinder-csi-plugin gcp-pd-csi-plugin local-path-provisioner local-volume-provisioner"
 
 # cd to the root directory of kubespray
 cd $(dirname $0)/../../
@@ -11,17 +11,17 @@ version_from_default=$(grep "^kube_version:" ./roles/kubespray-defaults/defaults
 version_from_readme=$(grep " \[kubernetes\]" ./README.md | awk '{print $3}')
 if [ "${version_from_default}" != "${version_from_readme}" ]; then
 	echo "The version of kubernetes is different between main.yml(${version_from_default}) and README.md(${version_from_readme})."
-	echo "If the pull request updates kubernetes version, please updates README.md also."
+	echo "If the pull request updates kubernetes version, please update README.md also."
 	exit 1
 fi
 
 for component in $(echo ${TARGET_COMPONENTS}); do
 	echo checking ${component}..
-	version_from_default=$(grep "^$(echo ${component} | sed s/"-"/"_"/)_version:" ./roles/download/defaults/main.yml | awk '{print $2}' | sed s/\"//g | sed s/^v//)
+	version_from_default=$(grep "^$(echo ${component} | sed s/"-"/"_"/g)_version:" ./roles/download/defaults/main.yml | awk '{print $2}' | sed s/\"//g | sed s/^v//)
 	version_from_readme=$(grep "\[${component}\]" ./README.md | grep "https" | awk '{print $3}' | sed s/^v//)
 	if [ "${version_from_default}" != "${version_from_readme}" ]; then
 		echo "The version of ${component} is different between main.yml(${version_from_default}) and README.md(${version_from_readme})."
-		echo "If the pull request updates ${component} version, please updates README.md also."
+		echo "If the pull request updates ${component} version, please update README.md also."
 		exit 1
 	fi
 done

1530411218f017fbd0b22dc5c9ec1c69961af1b1
Cristian Calin <6627509+cristicalin@users.noreply.github.com>
2022-10-19 15:47:05 +0300

    use cri-o from upstream instead of kubic/OBS (#9374)
    * [cri-o] use cri-o from upstream instead of kubic/OBS
    
    * [cri-o] add proper molecule coverage
    
    * [skopeo] download skopeo from upstream build
    
    * [cri-o] clean up legacy deployments
    
    * disable cri-o per-distribution variables

--- roles/container-engine/cri-o/molecule/default/tests/test_default.py
+++ roles/container-engine/cri-o/molecule/default/tests/test_default.py
@@ -19,3 +19,17 @@ def test_run(host):
         cmd = host.command(crictl + " --runtime-endpoint " + path + " version")
     assert cmd.rc == 0
     assert "RuntimeName:  cri-o" in cmd.stdout
+
+def test_run_pod(host):
+    runtime = "runc"
+
+    run_command = "/usr/local/bin/crictl run --with-pull --runtime {} /tmp/container.json /tmp/sandbox.json".format(runtime)
+    with host.sudo():
+        cmd = host.command(run_command)
+    assert cmd.rc == 0
+
+    with host.sudo():
+      log_f = host.file("/tmp/runc1.0.log")
+
+      assert log_f.exists
+      assert b"Hello from Docker" in log_f.content

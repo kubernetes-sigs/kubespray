00aa3a1ed8be2259e2cf6053d8a892db647012bf
Fredrik Liv <fredrik.liv@elastisys.com>
2023-01-05 13:16:00 +0100

    Added override variable, additional server groups and cloudinit config (#9452)

--- contrib/terraform/openstack/modules/compute/versions.tf
+++ contrib/terraform/openstack/modules/compute/versions.tf
@@ -4,5 +4,6 @@ terraform {
       source = "terraform-provider-openstack/openstack"
     }
   }
-  required_version = ">= 0.12.26"
+  experiments = [module_variable_optional_attrs]
+  required_version = ">= 0.14.0"
 }

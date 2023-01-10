ac28c71305a0631139eed53e2b2b99e651a23fe5
Cyclinder <qifeng.guo@daocloud.io>
2023-01-10 19:05:27 +0800

    feature request: add cyclinder to reviewers (#9646)
    Signed-off-by: cyclinder qifeng.guo@daocloud.io
    
    Signed-off-by: cyclinder qifeng.guo@daocloud.io

--- tests/scripts/check_typo.sh
+++ tests/scripts/check_typo.sh
@@ -9,4 +9,4 @@ set -e
 wget https://github.com/client9/misspell/releases/download/v0.3.4/misspell_0.3.4_linux_64bit.tar.gz
 tar -zxvf ./misspell_0.3.4_linux_64bit.tar.gz
 chmod 755 ./misspell
-git ls-files | xargs ./misspell -error
+git ls-files | grep -v OWNERS_ALIASES | xargs ./misspell -error

d7e7f6012ab81d7b9fc4a39103e95ea0c648540e
Mathieu Parent <math.parent@gmail.com>
2023-01-17 08:50:32 +0100

    Terraform vsphere cleanup (#9672)
    * contrib/terraform/vsphere: mark vsphere_password as sensitive
    
    Signed-off-by: Mathieu Parent <math.parent@gmail.com>
    
    * contrib/terraform/vsphere: remove unused modules
    
    Signed-off-by: Mathieu Parent <math.parent@gmail.com>

--- contrib/terraform/vsphere/versions.tf
+++ contrib/terraform/vsphere/versions.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/vsphere"
       version = ">= 1.24.3"
     }
-    null = {
-      source = "hashicorp/null"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
   }
   required_version = ">= 0.13"
 }

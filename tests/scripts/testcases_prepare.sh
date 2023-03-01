9322e4f19528f0641b779a2edfd2b5ac51086f7e
Kenichi Omichi <ken1ohmichi@gmail.com>
2023-03-01 09:00:46 +0900

    Drop part for supporting ansible 2.9 and 2.10
    requirements-$ANSIBLE_VERSION.yml doesn't exist in Kubespray repo.
    That was for supporting ansible 2.10-, and now Kubespray supports
    2.11+. So this drops the part to avoid confusion.

--- tests/scripts/testcases_prepare.sh
+++ tests/scripts/testcases_prepare.sh
@@ -9,10 +9,3 @@ mkdir -p /.ssh
 mkdir -p cluster-dump
 mkdir -p $HOME/.ssh
 ansible-playbook --version
-
-# in some cases we may need to bring in collections or roles from ansible-galaxy
-# to compensate for missing functionality in older ansible versions
-if [ -f requirements-${ANSIBLE_MAJOR_VERSION}.yml ] ; then
-  ansible-galaxy role install -r requirements-${ANSIBLE_MAJOR_VERSION}.yml
-  ansible-galaxy collection install -r requirements-${ANSIBLE_MAJOR_VERSION}.yml
-fi

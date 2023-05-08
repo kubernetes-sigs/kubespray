ab6d2046414027257bfba6469a9949079ef2bd84
Qasim Mehmood <18313886+ThisIsQasim@users.noreply.github.com>
2023-05-08 05:15:16 +0500

    Remove deprecated provider, fix flatcar configs, enable CI tests and refactor hetzner terraform  (#10002)
    * Remove deprecated provider and fix flatcar configs
    
    * Refactor for DRYness
    
    * Add missing line endings
    
    * Enable tests for hetzner terraform in CI
    
    * Add missing inventory for CI tests

--- contrib/terraform/hetzner/modules/kubernetes-cluster-flatcar/outputs.tf
+++ contrib/terraform/hetzner/modules/kubernetes-cluster-flatcar/outputs.tf
@@ -1,20 +1,22 @@
 output "master_ip_addresses" {
   value = {
-    for key, instance in hcloud_server.master :
-    instance.name => {
-      "private_ip" = hcloud_server_network.master[key].ip
-      "public_ip"  = hcloud_server.master[key].ipv4_address
-    }
+    for name, machine in var.machines :
+      name => {
+        "private_ip" = hcloud_server_network.machine[name].ip
+        "public_ip"  = hcloud_server.machine[name].ipv4_address
+      }
+      if machine.node_type == "master"
   }
 }
 
 output "worker_ip_addresses" {
   value = {
-    for key, instance in hcloud_server.worker :
-    instance.name => {
-      "private_ip" = hcloud_server_network.worker[key].ip
-      "public_ip"  = hcloud_server.worker[key].ipv4_address
-    }
+    for name, machine in var.machines :
+      name => {
+        "private_ip" = hcloud_server_network.machine[name].ip
+        "public_ip"  = hcloud_server.machine[name].ipv4_address
+      }
+      if machine.node_type == "worker"
   }
 }
 
--- contrib/terraform/hetzner/modules/kubernetes-cluster/versions.tf
+++ contrib/terraform/hetzner/modules/kubernetes-cluster/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
-      version = "1.31.1"
+      source  = "hetznercloud/hcloud"
+      version = "1.38.2"
     }
   }
   required_version = ">= 0.14"

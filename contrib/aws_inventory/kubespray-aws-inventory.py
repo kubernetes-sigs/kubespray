f25b6fce1ccff886e8bd6f50e2972fb64ed57669
Michael Stötzer <4034031+mstoetzer@users.noreply.github.com>
2023-06-02 07:12:52 +0200

    Add node_taints to aws_inventory script (#10168) (#10170)

--- contrib/aws_inventory/kubespray-aws-inventory.py
+++ contrib/aws_inventory/kubespray-aws-inventory.py
@@ -39,7 +39,7 @@ class SearchEC2Tags(object):
       hosts[group] = []
       tag_key = "kubespray-role"
       tag_value = ["*"+group+"*"]
-      region = os.environ['REGION']
+      region = os.environ['AWS_REGION']
 
       ec2 = boto3.resource('ec2', region)
       filters = [{'Name': 'tag:'+tag_key, 'Values': tag_value}, {'Name': 'instance-state-name', 'Values': ['running']}]
@@ -66,6 +66,11 @@ class SearchEC2Tags(object):
         node_labels_tag = list(filter(lambda t: t['Key'] == 'kubespray-node-labels', instance.tags))
         if node_labels_tag:
           ansible_host['node_labels'] = dict([ label.strip().split('=') for label in node_labels_tag[0]['Value'].split(',') ])
+
+        ##Set when instance actually has node_taints
+        node_taints_tag = list(filter(lambda t: t['Key'] == 'kubespray-node-taints', instance.tags))
+        if node_taints_tag:
+          ansible_host['node_taints'] = list([ taint.strip() for taint in node_taints_tag[0]['Value'].split(',') ])
 
         hosts[group].append(dns_name)
         hosts['_meta']['hostvars'][dns_name] = ansible_host

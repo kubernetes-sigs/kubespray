01ae6e2fa370baa584a9b1a0d45683670a6e89be
Fredrik Liv <fredrik.liv@elastisys.com>
2023-07-26 11:45:29 +0200

    terraform-openstack: Added missing provider

--- contrib/terraform/openstack/modules/compute/versions.tf
+++ contrib/terraform/openstack/modules/compute/versions.tf
@@ -3,6 +3,10 @@ terraform {
     openstack = {
       source = "terraform-provider-openstack/openstack"
     }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
   required_version = ">= 1.3.0"
 }

1955943d4aaab94fc477d4683f816b910bffcda7
Luke Simmons <luke.simmons@vgregion.se>
2023-08-14 14:11:21 +0200

    Removes Ansible reinstall from pipeline (#10032)

--- tests/scripts/testcases_prepare.sh
+++ tests/scripts/testcases_prepare.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-/usr/bin/python -m pip uninstall -y ansible ansible-base ansible-core
-/usr/bin/python -m pip install -r tests/requirements.txt
-ansible-galaxy install -r tests/requirements.yml
 mkdir -p /.ssh
 mkdir -p cluster-dump
 mkdir -p $HOME/.ssh

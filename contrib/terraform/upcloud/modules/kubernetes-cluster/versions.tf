370dbd22603d862a9a8e72b2cf9036155059867f
Robin Wallace <robin.wallace@elastisys.com>
2023-10-07 04:45:41 +0200

    upcloud: update terraform provider strict anti-affinity (#10474)

--- contrib/terraform/upcloud/modules/kubernetes-cluster/versions.tf
+++ contrib/terraform/upcloud/modules/kubernetes-cluster/versions.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     upcloud = {
-      source = "UpCloudLtd/upcloud"
-      version = "~>2.7.1"
+      source  = "UpCloudLtd/upcloud"
+      version = "~>2.12.0"
     }
   }
   required_version = ">= 0.13"

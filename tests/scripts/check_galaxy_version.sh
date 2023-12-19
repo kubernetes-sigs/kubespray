e2996089118e2743a480ca6e5cf68c753ba52bd6
Kay Yan <kay.yan@daocloud.io>
2023-12-19 02:02:10 +0000

    update galaxy and CI

--- tests/scripts/check_galaxy_version.sh
+++ tests/scripts/check_galaxy_version.sh
@@ -4,9 +4,9 @@ set -e
 version_from_galaxy=$(grep "^version:" galaxy.yml | awk '{print $2}')
 version_from_docs=$(grep -P "^\s+version:\sv\d+\.\d+\.\d+" docs/ansible_collection.md | awk '{print $2}')
 
-if [[ $KUBESPRAY_VERSION != "v${version_from_galaxy}" ]]
+if [[ $KUBESPRAY_VERSION == "v${version_from_galaxy}" ]]
 then
-	echo "Please update galaxy.yml version to match the KUBESPRAY_VERSION. Be sure to remove the \"v\" to adhere"
+	echo "Please update galaxy.yml version to the next release of KUBESPRAY_VERSION. Be sure to remove the \"v\" to adhere"
 	echo "to semenatic versioning"
 	exit 1
 fi

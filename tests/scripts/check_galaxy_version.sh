45e32edb1969ba4da9dd3657555a6866a6b80194
Max Gautier <mg@max.gautier.name>
2023-12-19 10:23:47 +0100

    Check that we upgraded galaxy.yml to next version
    This is only intented to check for human error. The version in galaxy
    should be the next (which does not mean the same if we're on master or a
    release branch).

--- tests/scripts/check_galaxy_version.sh
+++ tests/scripts/check_galaxy_version.sh
@@ -3,9 +3,10 @@ set -e
 
 version_from_galaxy=$(grep "^version:" galaxy.yml | awk '{print $2}')
 
-if [[ $KUBESPRAY_VERSION != "v${version_from_galaxy}" ]]
+# TODO: compute the next expected version somehow
+if [[ $KUBESPRAY_VERSION == "v${version_from_galaxy}" ]]
 then
-	echo "Please update galaxy.yml version to match the KUBESPRAY_VERSION. Be sure to remove the \"v\" to adhere"
-	echo "to semenatic versioning"
+	echo "Please update galaxy.yml version to match the next KUBESPRAY_VERSION."
+	echo "Be sure to remove the \"v\" to adhere to semantic versioning"
 	exit 1
 fi

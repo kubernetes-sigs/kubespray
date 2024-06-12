cf4e02221268b92602d89cf780ba95f8fd51ac48
ant31 <2t.antoine@gmail.com>
2024-06-12 10:18:44 +0200

    Test

--- tests/scripts/vagrant-install.sh
+++ tests/scripts/vagrant-install.sh
@@ -1,44 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
-install_vagrant() {
- sudo apt install vagrant-libvirt vagrant -y
- sudo vagrant plugin install vagrant-libvirt
-}
+# install_vagrant() {
+#  sudo apt install vagrant-libvirt vagrant -y
+#  sudo vagrant plugin install vagrant-libvirt
+# }
 
-prep(){
-	sudo apt-get update -y
-	sudo apt-get install ca-certificates curl libvirt-daemon-system\
-		libvirt-clients qemu-utils qemu-kvm htop atop -y
+# prep(){
+# 	sudo apt-get update -y
+# 	sudo apt-get install ca-certificates curl libvirt-daemon-system\
+# 		libvirt-clients qemu-utils qemu-kvm htop atop -y
 
-	sudo install -m 0755 -d /etc/apt/keyrings
-}
-install_docker() {
-	VERSION_STRING=5:26.1.0-1~ubuntu.24.04~noble
-	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-	sudo chmod a+r /etc/apt/keyrings/docker.asc
+# 	sudo install -m 0755 -d /etc/apt/keyrings
+# }
+# install_docker() {
+# 	VERSION_STRING=5:26.1.0-1~ubuntu.24.04~noble
+# 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# 	sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-	# Add the repository to Apt sources:
-	echo \
-		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-			sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update -y
+# 	# Add the repository to Apt sources:
+# 	echo \
+# 		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+# 		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+# 			sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# 	sudo apt-get update -y
 
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-}
-install_docker_auto () {
-	 curl -fsSL https://get.docker.com -o get-docker.sh
-	 sudo sh ./get-docker.sh --dry-run
-}
-do_install() {
-	install_vagrant
-	install_docker
-
-}
+# 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# }
+# install_docker_auto () {
+# 	 curl -fsSL https://get.docker.com -o get-docker.sh
+# 	 sudo sh ./get-docker.sh --dry-run
+# }
 
 
 
-VAGRANT_VERSION=2.3.7
+VAGRANT_VERSION=2.4.1
 VAGRANT_DEFAULT_PROVIDER=libvirt
 VAGRANT_ANSIBLE_TAGS=facts
 LANG=C.UTF-8
@@ -46,40 +41,67 @@ DEBIAN_FRONTEND=noninteractive
 PYTHONDONTWRITEBYTECODE=1
 KUBE_VERSION=1.29.5
 pipeline_install() {
-apt update -q \
-apt install -yq \
-         libssl-dev \
-         python3-dev \
-         python3-pip \
-         sshpass \
-         apt-transport-https \
-         jq \
-         moreutils \
-         libvirt-dev \
-         openssh-client \
-         rsync \
-         git \
-         ca-certificates \
-         curl \
-         gnupg2 \
-         software-properties-common \
-         unzip \
-         libvirt-clients \
-         qemu-utils
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-apt update -q
-apt install --no-install-recommends -yq docker-ce
-apt autoremove -yqq --purge && apt clean && rm -rf /var/lib/apt/lists/* /var/log/*
+    cp /etc/apt/sources.list /etc/apt/sources.list."$(date +"%F")"
+    sed -i -e '/^# deb-src.*universe$/s/# //g' /etc/apt/sources.list
 
-curl -L https://dl.k8s.io/release/$KUBE_VERSION/bin/linux/$(dpkg --print-architecture)/kubectl -o /usr/local/bin/kubectl \
-echo $(curl -L https://dl.k8s.io/release/$KUBE_VERSION/bin/linux/$(dpkg --print-architecture)/kubectl.sha256) /usr/local/bin/kubectl | sha256sum --check \
-chmod a+x /usr/local/bin/kubectl
+    apt update
+    # libssl-dev \
+        # python3-dev \
+        #         # jq \
+        # moreutils \
+        # libvirt-dev \
+        #         # rsync \
+        # git \
+        #                 # htop \
+        # gpg \
+        # atop
+
+        # gnupg2 \
+# software-properties-common \
+    apt install --no-install-recommends -y \
+        git \
+        make \
+        python3-pip \
+        sshpass \
+        apt-transport-https \
+        openssh-client \
+        ca-certificates \
+        curl \
+        libfuse2 \
+        unzip \
+        libvirt-clients \
+        qemu-utils \
+        libvirt-daemon-system \
+        libvirt-clients \
+        qemu-kvm
+
+    ### DOCKER ###
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt update
+    apt install --no-install-recommends -y docker-ce
+    apt autoremove -y --purge && apt clean && rm -rf /var/lib/apt/lists/* /var/log/*
+
+    ### KUBECTL ###
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    mv kubectl /usr/local/bin/kubectl
+    chmod a+x /usr/local/bin/kubectl
+
     # Install Vagrant
-curl -LO https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}-1_$(dpkg --print-architecture).deb
-dpkg -i vagrant_${VAGRANT_VERSION}-1_$(dpkg --print-architecture).deb
-rm vagrant_${VAGRANT_VERSION}-1_$(dpkg --print-architecture).deb
-vagrant plugin install vagrant-libvirt
+     # apt update -y
+    # echo apt-get install -y unzip libfuse2 vagrant vagrant-libvirt
+    # apt --fix-broken install -y
+    # dpkg --configure -a -y
+
+    ### VAGRANT ###
+    # apt-get install -y unzip
+    curl -LO https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_linux_amd64.zip
+    unzip vagrant_${VAGRANT_VERSION}_linux_amd64.zip
+    mv vagrant /usr/local/bin/vagrant
+    chmod a+x /usr/local/bin/vagrant
+    # ls -la /usr/local/bin/vagrant
+    /usr/local/bin/vagrant plugin install vagrant-libvirt
+
 }
 # wrapped up in a function so that we have some protection against only getting
 # half the file during "curl | sh"

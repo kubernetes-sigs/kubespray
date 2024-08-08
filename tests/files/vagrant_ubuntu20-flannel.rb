09347d8b9bad588258f32307567676d48fa10232
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2024-08-07 18:52:01 -0700

    [release-2.25] CI: rework pipeline: short/extended based on labels  (#11424)
    * CI: reduce VM resources requests to improve scheduling
    
    * CI: Reduce default jobs; add labels(ci-full/extended) to run more test
    
    * CI: use jobs dependencies instead of stages
    
    * precommit one-job
    
    * CI: Use Kubevirt VM to run Molecule and Vagrant jobs
    
    ---------
    
    Co-authored-by: ant31 <2t.antoine@gmail.com>

--- tests/files/vagrant_ubuntu20-flannel.rb
+++ tests/files/vagrant_ubuntu20-flannel.rb
@@ -2,7 +2,6 @@ $os = "ubuntu2004"
 
 # For CI we are not worries about data persistence across reboot
 $libvirt_volume_cache = "unsafe"
-
 # Checking for box update can trigger API rate limiting
 # https://www.vagrantup.com/docs/vagrant-cloud/request-limits.html
 $box_check_update = false

b981e2f740a2418def3e82bfe241c6baaaad71d9
ChengHao Yang <17496418+tico88612@users.noreply.github.com>
2025-06-10 21:44:25 +0800

    Replace terraform with opentf (#12291)
    Terraform is no longer open source software and has been removed and
    replaced with OpenTofu.
    
    Signed-off-by: ChengHao Yang <17496418+tico88612@users.noreply.github.com>

---
+++ tests/scripts/opentofu_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euxo pipefail
+
+curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+chmod +x install-opentofu.sh
+./install-opentofu.sh --install-method standalone
+rm -f install-opentofu.sh
+tofu --version

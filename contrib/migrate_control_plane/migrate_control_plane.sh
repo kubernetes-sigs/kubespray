f3e647aaa234fa10a662c16acc06cc5937d35ae1
Max Gautier <mg@max.gautier.name>
2025-10-10 14:38:48 +0200

    Add contrib script to rotate control plane to new nodes

---
+++ contrib/migrate_control_plane/migrate_control_plane.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ansible-inventory $@ --list > /tmp/base_inventory.json 
+
+NUMBER_OF_CONTROL_PLANE=$(jq < /tmp/base_inventory.json '[(.kube_control_plane.hosts | length), (.etcd.hosts | length)] | max')
+
+export BASE_INVENTORY_FILE=/tmp/base_inventory.json
+export ANSIBLE_INVENTORY=rotate_inventory.py
+
+# Delete last control plane
+INDEX=0 ansible -m debug -a var=node -e node='{{ groups.kube_control_plane[-1] }},{{ groups.etcd[-1]}}' kube_control_plane,etcd
+
+# Add new control plane (last place), will be switched to first place in the first loop iteration
+ROTATION=1 INDEX=1 ansible -m debug -a var=inventory_hostname kube_control_plane,etcd
+
+for ((index=1;index<NUMBER_OF_CONTROL_PLANE;index++))
+do
+    # add new control plane / reorder
+    INDEX=$index ansible -m debug -a var=inventory_hostname kube_control_plane,etcd
+    # remove old control plane
+    INDEX=$index ansible -m debug -a var=node -e node=node='{{ groups.kube_control_plane[-1] }},{{ groups.etcd[-1]}}' kube_control_plane,etcd
+done
+
+# Last iteration is done separately because we don't need a remove-node step.
+INDEX=$index ansible -m debug -a var=inventory_hostname kube_control_plane,etcd

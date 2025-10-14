119ae7efb2f98de35289bacaf405b94abb998d66
Max Gautier <mg@max.gautier.name>
2025-10-14 11:32:44 +0200

    Fix removal of control plane
    Filter the removed nodes to avoid removing the new ones when etcd and
    kube_control_plane have a different number of nodes.

--- contrib/migrate_control_plane/migrate_control_plane.sh
+++ contrib/migrate_control_plane/migrate_control_plane.sh
@@ -8,18 +8,17 @@ export BASE_INVENTORY_FILE=/tmp/base_inventory.json
 export ANSIBLE_INVENTORY=rotate_inventory.py
 
 # Delete last control plane
-INDEX=0 ansible -m debug -a var=node -e node='{{ groups.kube_control_plane[-1] }},{{ groups.etcd[-1]}}' kube_control_plane,etcd
-
+INDEX=0 ansible -m debug -a var=node -e node='{{ [groups.kube_control_plane[-1], groups.etcd[-1] ] | reject("in", groups.new_etcd + groups.new_kube_control_plane) }}'  kube_control_plane[0]
 # Add new control plane (last place), will be switched to first place in the first loop iteration
-ROTATION=1 INDEX=1 ansible -m debug -a var=inventory_hostname kube_control_plane,etcd
-
+ROTATION=1 INDEX=1 ansible -m debug -a msg=upgrade kube_control_plane,etcd
 for ((index=1;index<NUMBER_OF_CONTROL_PLANE;index++))
 do
     # add new control plane / reorder
-    INDEX=$index ansible -m debug -a var=inventory_hostname kube_control_plane,etcd
+    INDEX=$index ansible -m debug -a msg=upgrade kube_control_plane,etcd
     # remove old control plane
-    INDEX=$index ansible -m debug -a var=node -e node=node='{{ groups.kube_control_plane[-1] }},{{ groups.etcd[-1]}}' kube_control_plane,etcd
+    INDEX=$index ansible -m debug -a var=node -e node='{{ [groups.kube_control_plane[-1], groups.etcd[-1] ] | reject("in", groups.new_etcd + groups.new_kube_control_plane) }}'  kube_control_plane[0]
+
 done
 
 # Last iteration is done separately because we don't need a remove-node step.
-INDEX=$index ansible -m debug -a var=inventory_hostname kube_control_plane,etcd
+INDEX=$index ansible -m debug -a msg=upgrade kube_control_plane,etcd

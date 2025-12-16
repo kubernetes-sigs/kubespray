51ccd2337790e93a65b044be40d6f895682ea56c
Max Gautier <mg@max.gautier.name>
2025-12-16 11:40:31 +0100

    rotate_inventory.py: don't touch if there is no 'hosts' key
    If etcd is defined as a parent of other groups (typically,
    `kube_control_plane`), it's simpler to just pass that along to ansible
    and rely on the rotation for that group.

--- contrib/migrate_control_plane/rotate_inventory.py
+++ contrib/migrate_control_plane/rotate_inventory.py
@@ -16,10 +16,22 @@ kube_cp = (
 )
 kube_cp = kube_cp[rotation:] + kube_cp[:rotation]
 inventory["kube_control_plane"]["hosts"] = kube_cp
-etcd_cp = (
-    inventory["new_etcd"]["hosts"][:index]
-    + inventory["etcd"]["hosts"][slice(None, None if index == 0 else -index)]
-)
-etcd_cp = etcd_cp[rotation:] + etcd_cp[:rotation]
-inventory["etcd"]["hosts"] = etcd_cp
+if "hosts" in inventory["new_etcd"] and "hosts" in inventory["etcd"]:
+    etcd_cp = (
+        inventory["new_etcd"]["hosts"][:index]
+        + inventory["etcd"]["hosts"][slice(None, None if index == 0 else -index)]
+    )
+    etcd_cp = etcd_cp[rotation:] + etcd_cp[:rotation]
+    inventory["etcd"]["hosts"] = etcd_cp
+elif "hosts" in inventory["new_etcd"] or "hosts" in inventory["etcd"]:
+    print(
+        """
+    'etcd' and 'new_etcd' are defined differently (with 'hosts' / not with 'hosts').
+    This could produce unexpected results.
+    Aborting inventory.
+    """
+    )
+    exit(1)
+else:
+    print("etcd hosts not explicity defined, not modifying", file=sys.stderr)
 json.dump(inventory, fp=sys.stdout)

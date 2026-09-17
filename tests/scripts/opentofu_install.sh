1c3f41ce45a3f3f7a69483d903222af0c5b26d0b
xoxonut <frank.ankch17@gmail.com>
2026-09-17 23:30:04 +0800

    ci: run the Ansible controller as an unprivileged user

--- tests/scripts/opentofu_install.sh
+++ tests/scripts/opentofu_install.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 
 curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
 chmod +x install-opentofu.sh
-./install-opentofu.sh --install-method standalone
+./install-opentofu.sh --install-method standalone --install-path "${HOME}/.local/bin" --symlink-path - --root-method none
 rm -f install-opentofu.sh
 tofu --version
